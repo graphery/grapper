24e9623ebc01d1cef52c27e0a1aad99276fdef12
pabloalmunia <pablo.almunia@corusconsulting.com>
2025-08-08 14:04:23 +0200

    Move src/helpers to src/core/helpers

--- dist/module/plugins/tosource.js
+++ dist/module/plugins/tosource.js
@@ -1,1 +1,1 @@
-/* grapper - 1.0.1 [module:plugin.tosource] */ function t(){return new XMLSerializer().serializeToString(this.el).replaceAll("<!-- ref -->","")}function n(e){e.extendInstance({toSource:t})}var l=n;export{l as default};
+/* grapper - 1.0.0-beta.1 [module:plugin.tosource] */ function t(){return new XMLSerializer().serializeToString(this.el).replaceAll("<!-- ref -->","")}function n(e){e.extendInstance({toSource:t})}var l=n;export{l as default};
