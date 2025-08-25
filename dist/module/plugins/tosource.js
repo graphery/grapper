b37236c6077fe9373d69bac59f048a4d86a21e41
pabloalmunia <pablo.almunia@corusconsulting.com>
2025-08-25 20:16:02 +0200

    Bump version to 1.1.0-beta.6, add getters for `svg`, `data`, `config`, and `methods` for fix access to `$.` from methods.

--- dist/module/plugins/tosource.js
+++ dist/module/plugins/tosource.js
@@ -1,1 +1,1 @@
-/* grapper - 1.1.0-beta.5 [module:plugin.tosource] */ function t(){return new XMLSerializer().serializeToString(this.el).replaceAll("<!-- ref -->","")}function n(e){e.extendInstance({toSource:t})}var l=n;export{l as default};
+/* grapper - 1.1.0-beta.6 [module:plugin.tosource] */ function t(){return new XMLSerializer().serializeToString(this.el).replaceAll("<!-- ref -->","")}function n(e){e.extendInstance({toSource:t})}var l=n;export{l as default};
